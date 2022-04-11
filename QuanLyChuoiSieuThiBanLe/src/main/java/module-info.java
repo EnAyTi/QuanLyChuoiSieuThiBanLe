76ff836c0d62eea2cadc26879062c812a476426c
enayti <anhtuan222001@gmail.com>
2022-04-11 16:34:41 +0700

    tao project

---
+++ QuanLyChuoiSieuThiBanLe/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.oumarket.quanlychuoisieuthibanle {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+    requires java.sql;
+
+    opens com.oumarket.quanlychuoisieuthibanle to javafx.fxml;
+    exports com.oumarket.quanlychuoisieuthibanle;
+}
